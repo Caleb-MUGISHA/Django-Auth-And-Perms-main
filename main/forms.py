cead03a9074c7c1fefb55769710f1181b568736c
Caleb-MUGISHA <c.mugisha@alustdent.com>
2022-10-06 16:02:59 +0200

    done

--- main/forms.py
+++ main/forms.py
@@ -1,15 +1,25 @@
+from email.policy import default
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 
-
+ROLES= (
+    ('Student','Student'),
+    ('Facilitator','Facilitator'),
+    ('Team Lead','Team Lead'),
+   
+    )
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    role=forms.ChoiceField(choices=ROLES)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "role","password1", "password2"]
+        
+       
+       
 
 
 class PostForm(forms.ModelForm):
